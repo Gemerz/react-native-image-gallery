dcf4de46bb01460b9efba3c8c870363fd994e53a
gemer cheung <gemercheung@gmail.com>
2018-12-12 13:25:55 +0800

    fixed keyExtractor

--- src/libraries/ViewPager/index.js
+++ src/libraries/ViewPager/index.js
@@ -260,7 +260,7 @@ export default class ViewPager extends PureComponent {
     }
 
     keyExtractor (item, index) {
-        return index;
+        return `${item}-${index}`;
     }
 
     renderRow ({ item, index }) {
